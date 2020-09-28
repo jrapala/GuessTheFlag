31c3936def9f4fb7c5a81fe11f99597a6989de31
Juliette Rapala <jrapala@gmail.com>
2020-09-27 21:08:38 -0500

    refactor: extract flag images to separate component

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -34,11 +34,7 @@ struct ContentView: View {
                     Button(action: {
                         self.flagTapped(number)
                     }) {
-                        Image(self.countries[number])
-                            .renderingMode(.original)
-                            .clipShape(Capsule())
-                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
-                            .shadow(color: .black, radius: 2)
+                        FlagImage(imageName: self.countries[number])
                     }
                 }
                 
@@ -75,6 +71,18 @@ struct ContentView: View {
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+    }
+}
+
+struct FlagImage: View {
+    var imageName: String
+
+    var body: some View {
+        Image(imageName)
+            .renderingMode(.original)
+            .clipShape(Capsule())
+            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+            .shadow(color: .black, radius: 2)
     }
 }
 
